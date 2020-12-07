1aacc43410741b5586bb80fde0880e98cac8d140
Ashwani Aggarwal <agrawash@Ashwanis-MBP.fios-router.home>
2020-12-07 16:52:09 -0500

    updated Nav styles and added cart icon

--- src/app/components/Product.js
+++ src/app/components/Product.js
@@ -1,25 +1,35 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const Product = ({ title, detail, inCart, updateCount }) => {
-
-  const[isInCart, setIsInCart] = useState(inCart);
+  const [isInCart, setIsInCart] = useState(inCart);
   const handleAddToCart = () => {
     setIsInCart(true);
     updateCount();
-
-  }
+  };
   return (
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center ">
-      <div className="flex-shrink-0">
-        <img className="h-12 w-12" src="../img/logo.png" alt="Logo" />
+    <div>
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center ">
+        <div className="flex-shrink-0">
+          <img className="h-12 w-12" src="/img/logo.png" alt="Logo" />
+        </div>
+        <div>
+          <div className="text-xl font-medium text-black">{title}</div>
+          <p className="text-gray-500">{detail}</p>
+        </div>
+        <div className="relative -right-7 -bottom-7 text-sm p-3">
+        {isInCart ? (
+          <span className=" bg-gray-500 p-1 rounded-full text-white ">In Cart</span>
+        ) : (
+          <button
+            className=" bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white "
+            onClick={handleAddToCart}
+          >
+            Add to Cart
+          </button>
+        )}
       </div>
-      <div>
-        <div className="text-xl font-medium text-black">{title}</div>
-        <p className="text-gray-500">{detail}</p>
       </div>
-      <div>
-        {isInCart ? <span>In Cart</span> : <button onClick={handleAddToCart}>Add to Cart</button>}
-      </div>
+
     </div>
   );
 };
