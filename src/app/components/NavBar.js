801b21694665a340f00e360f804779d9877440af
Gaurav Thakur <gaurav.thakur@yash.com>
2020-12-07 20:35:06 +0530

    Incart counter

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const NavBar = ()=><div>I am navbar</div>
+const NavBar = ({ inCartCount }) => (
+  <div className="bg-blue-500 p-1 w-100 text-white">
+    I am navbar
+    <div className="bg-red-500 p-1 text-white">
+        Incart Items {inCartCount}
+    </div>
+  </div>
+);
 
 export default NavBar;
--- src/app/components/Product.js
+++ src/app/components/Product.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Product = ({ title, detail }) => {
+const Product = ({ title, detail, inCart, updateCount }) => {
+  
+  const [isInCart, setIsInCart] = useState(inCart);
+
+  const handleAddToCart = () => {
+    setIsInCart(true);
+    updateCount();
+  };
+
   return (
     <div>
       <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
@@ -11,6 +19,15 @@ const Product = ({ title, detail }) => {
           <div className="text-xl font-medium text-black">{title}</div>
           <p className="text-gray-500">{detail}</p>
         </div>
+      </div>
+      <div>
+        {isInCart ? (
+          <span className="bg-blue-500 text-white p-1">In Cart</span>
+        ) : (
+          <button className="bg-blue-500 text-white p-1" onClick={handleAddToCart}>
+            Add to Cart
+          </button>
+        )}
       </div>
     </div>
   );
